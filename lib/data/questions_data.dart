ffbfc631eda34a700196ba45e7041dc21b2062dd
VuittonVU <vuittonvarianu@gmail.com>
2025-11-01 16:44:40 +0700

    chore(structure): add data folder dan memindah questions ke file tersendiri

---
+++ lib/data/questions_data.dart
@@ -0,0 +1,158 @@
+const List<Map<String, dynamic>> easyQuestions = [
+  {
+    'question': 'Planet terbesar di tata surya adalah?',
+    'options': ['Bumi', 'Saturnus', 'Jupiter', 'Mars'],
+    'answer': 'Jupiter',
+  },
+  {
+    'question': 'Negara mana yang dijuluki “Negeri Sakura”?',
+    'options': ['Korea Selatan', 'Tiongkok', 'Jepang', 'Thailand'],
+    'answer': 'Jepang',
+  },
+  {
+    'question': 'Siapa penemu bola lampu?',
+    'options': ['Nikola Tesla', 'Isaac Newton', 'Albert Einstein', 'Thomas Edison'],
+    'answer': 'Thomas Edison',
+  },
+  {
+    'question': 'Gunung tertinggi di dunia adalah?',
+    'options': ['Kilimanjaro', 'Everest', 'Elbrus', 'Denali'],
+    'answer': 'Everest',
+  },
+  {
+    'question': 'Hewan tercepat di darat adalah?',
+    'options': ['Singa', 'Kuda', 'Cheetah', 'Serigala'],
+    'answer': 'Cheetah',
+  },
+  {
+    'question': 'Ibukota Australia adalah?',
+    'options': ['Sydney', 'Melbourne', 'Canberra', 'Perth'],
+    'answer': 'Canberra',
+  },
+  {
+    'question': 'Film “Titanic” disutradarai oleh?',
+    'options': ['James Cameron', 'Steven Spielberg', 'Christopher Nolan', 'Ridley Scott'],
+    'answer': 'James Cameron',
+  },
+  {
+    'question': 'Unsur kimia dengan simbol “O” adalah?',
+    'options': ['Osmium', 'Oxygen', 'Oganesson', 'Oxide'],
+    'answer': 'Oxygen',
+  },
+  {
+    'question': 'Benua terbesar di dunia adalah?',
+    'options': ['Afrika', 'Asia', 'Amerika', 'Eropa'],
+    'answer': 'Asia',
+  },
+  {
+    'question': 'Bahasa resmi di Brasil adalah?',
+    'options': ['Spanyol', 'Portugis', 'Prancis', 'Italia'],
+    'answer': 'Portugis',
+  },
+];
+
+const List<Map<String, dynamic>> mediumQuestions = [
+  {
+    'question': 'Siapa ilmuwan yang mengemukakan teori relativitas?',
+    'options': ['Isaac Newton', 'Albert Einstein', 'Stephen Hawking', 'Galileo Galilei'],
+    'answer': 'Albert Einstein',
+  },
+  {
+    'question': 'Apa nama unsur kimia dengan simbol “Na”?',
+    'options': ['Nitrogen', 'Natrium', 'Neon', 'Nickel'],
+    'answer': 'Natrium',
+  },
+  {
+    'question': 'Negara dengan populasi terbanyak di dunia adalah?',
+    'options': ['India', 'Tiongkok', 'Amerika Serikat', 'Indonesia'],
+    'answer': 'Tiongkok',
+  },
+  {
+    'question': 'Siapa penulis novel “Harry Potter”?',
+    'options': ['Suzanne Collins', 'J.K. Rowling', 'Stephen King', 'George R.R. Martin'],
+    'answer': 'J.K. Rowling',
+  },
+  {
+    'question': 'Gunung berapi tertinggi di Indonesia adalah?',
+    'options': ['Kerinci', 'Rinjani', 'Semeru', 'Agung'],
+    'answer': 'Kerinci',
+  },
+  {
+    'question': 'Hewan apa yang bisa hidup di darat dan air?',
+    'options': ['Kadal', 'Katak', 'Ular', 'Penyu'],
+    'answer': 'Katak',
+  },
+  {
+    'question': 'Negara yang menjadi tempat kelahiran Olimpiade adalah?',
+    'options': ['Italia', 'Yunani', 'Mesir', 'Prancis'],
+    'answer': 'Yunani',
+  },
+  {
+    'question': 'Apa nama planet terdekat dari Matahari?',
+    'options': ['Venus', 'Merkurius', 'Mars', 'Bumi'],
+    'answer': 'Merkurius',
+  },
+  {
+    'question': 'Siapa yang pertama kali menginjakkan kaki di bulan?',
+    'options': ['Buzz Aldrin', 'Neil Armstrong', 'Yuri Gagarin', 'Michael Collins'],
+    'answer': 'Neil Armstrong',
+  },
+  {
+    'question': 'Satuan listrik “Ohm” digunakan untuk mengukur?',
+    'options': ['Arus', 'Hambatan', 'Tegangan', 'Daya'],
+    'answer': 'Hambatan',
+  },
+];
+
+const List<Map<String, dynamic>> hardQuestions = [
+  {
+    'question': 'Siapa pelukis dari lukisan “The Starry Night”?',
+    'options': ['Pablo Picasso', 'Leonardo da Vinci', 'Vincent van Gogh', 'Claude Monet'],
+    'answer': 'Vincent van Gogh',
+  },
+  {
+    'question': 'Teori mekanika kuantum pertama kali dikembangkan oleh?',
+    'options': ['Niels Bohr', 'Max Planck', 'Werner Heisenberg', 'Erwin Schrödinger'],
+    'answer': 'Max Planck',
+  },
+  {
+    'question': 'Negara pertama yang menggunakan hak pilih perempuan?',
+    'options': ['Selandia Baru', 'Kanada', 'Swiss', 'Finlandia'],
+    'answer': 'Selandia Baru',
+  },
+  {
+    'question': 'Apa nama partikel penyusun atom yang bermuatan negatif?',
+    'options': ['Proton', 'Neutron', 'Elektron', 'Positron'],
+    'answer': 'Elektron',
+  },
+  {
+    'question': 'Siapa yang menemukan teori evolusi melalui seleksi alam?',
+    'options': ['Gregor Mendel', 'Charles Darwin', 'Louis Pasteur', 'Jean Lamarck'],
+    'answer': 'Charles Darwin',
+  },
+  {
+    'question': 'Simbol kimia untuk emas adalah?',
+    'options': ['Ag', 'Au', 'Fe', 'Pt'],
+    'answer': 'Au',
+  },
+  {
+    'question': 'Karya sastra “Odyssey” ditulis oleh?',
+    'options': ['Sophocles', 'Plato', 'Homer', 'Aristotle'],
+    'answer': 'Homer',
+  },
+  {
+    'question': 'Proses perubahan padat menjadi gas disebut?',
+    'options': ['Kondensasi', 'Sublimasi', 'Evaporasi', 'Deposisi'],
+    'answer': 'Sublimasi',
+  },
+  {
+    'question': 'Unit internasional untuk mengukur energi adalah?',
+    'options': ['Newton', 'Joule', 'Pascal', 'Watt'],
+    'answer': 'Joule',
+  },
+  {
+    'question': 'Negara mana yang memiliki populasi terbanyak di Eropa?',
+    'options': ['Jerman', 'Prancis', 'Rusia', 'Inggris'],
+    'answer': 'Rusia',
+  },
+];
